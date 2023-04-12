2d815b230cf9fe5635818f1a7b6c181980cda827
Bhavana <115468261+bhavanakoguru@users.noreply.github.com>
2023-04-13 03:56:31 +0530

    factorial of a number

---
+++ week_01/day_2/day2_9.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class day2_9
+{
+	public static void main(String[] args)
+	{
+		Scanner obj=new Scanner(System.in);
+		System.out.print("Enter your number:");
+		int n=obj.nextInt();
+		int count=1;
+		for(int i=n;i>0;i--)
+		{
+		 	count *=i;
+		}
+		System.out.println(n+" Factorial is: "+count);
+	}
+}
