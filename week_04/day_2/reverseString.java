f923c08168a87f190037dba6258db669f669f11a
Bhavana <kogurubhavana@gmail.com>
2023-08-23 15:02:47 +0530

    Reverse String

---
+++ week_04/day_2/reverseString.java
@@ -0,0 +1,13 @@
+class reverseString
+{
+	public static void main(String[] args)
+	{
+		String str="Bhavana";
+		String reverse="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			reverse+=str.charAt(i);
+		}
+		System.out.println(reverse);
+	}
+}
