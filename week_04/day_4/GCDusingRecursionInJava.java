dac1a8f8e8f5885e592f0e69bc86b35d0e72a826
Bhavana <kogurubhavana@gmail.com>
2023-08-24 23:29:20 +0530

    GCD using Recursion

---
+++ week_04/day_4/GCDusingRecursionInJava.java
@@ -0,0 +1,34 @@
+class GCDusingRecursionInJava
+{
+	public static void main(String[] args)
+	{
+		GCD(10,120,0,1);
+	}
+	
+	public static void GCD(int n1,int n2,int ans,int i)
+	{
+		
+		for(i=1;i<n1+n2;i++)
+		{
+			
+			if(n1%i==0 && n2%i==0)
+			{
+				ans=i;
+			}
+		}
+		System.out.println(ans);
+		
+		//Recursion..
+		
+		if(i<(n1+n2))
+		{
+			if(n1%i == 0 && n2%i == 0)
+			{
+				ans=i;
+			}
+			GCD(n1,n2,ans,++i);
+			return;
+		}
+		System.out.println(ans);	
+	}
+}
