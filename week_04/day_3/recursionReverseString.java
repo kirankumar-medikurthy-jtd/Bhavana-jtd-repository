d6b402e2ba2e889b03cd952570ff1cab6fde493d
Bhavana <kogurubhavana@gmail.com>
2023-08-24 23:10:19 +0530

    reversing string using recursion

---
+++ week_04/day_3/recursionReverseString.java
@@ -0,0 +1,18 @@
+class recursionReverseString
+{
+	public static void main(String[] args)
+	{
+		String str="Bhavana";
+		reverseString(str,str.length()-1,"",7);
+	}
+	public static void reverseString(String str,int i,String bag,int size)
+	{
+		if(i<0)
+		{
+			System.out.println("Reverse String : "+bag);
+			return;
+		}
+		bag+=str.charAt(i);
+		reverseString(str,--i,bag,size);
+	}
+}
